6edae5dd6e50b508d8714fc903eae0ab8d8f62dd
haeyong9701 <haeyong9701@gmail.com>
2023-12-16 21:02:28 +0900

    add: 모듈 이름 붙이기

--- 12월/모듈/index.js
+++ 12월/모듈/index.js
@@ -1,3 +1,5 @@
-import { title, print } from "./printer.js";
+import { title as printTitle, print } from "./printer.js";
+import { title, data as members } from "./member.js";
 
 print(title);
+print(members);
---
+++ 12월/모듈/member.js
@@ -0,0 +1,3 @@
+export const title = "CodeitMembers";
+
+export const data = ["영훈", "윤수", "동욱", "영준", "규식", "태호", "지웅", "효신"];
