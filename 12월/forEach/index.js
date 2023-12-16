23d623496781b01ece08da39aa304806104d5c9c
haeyong9701 <haeyong9701@gmail.com>
2023-12-16 11:35:30 +0900

    add: forEach로 dom 작성.js

---
+++ 12월/forEach/index.js
@@ -0,0 +1,31 @@
+const list = document.querySelector(".list");
+const data = [
+  {
+    title: "자바스크립트 공부하기",
+    isClear: true,
+  },
+  {
+    title: "쓰레기 분리수거",
+    isClear: false,
+  },
+  {
+    title: "고양이 밥주기",
+    isClear: true,
+  },
+  {
+    title: "독서하기",
+    isClear: false,
+  },
+  {
+    title: "영어 공부하기",
+    isClear: false,
+  },
+];
+
+// 여기에 코드를 작성해 주세요.
+data.forEach((data, i) => {
+  const todo = document.createElement("li");
+  todo.classList.add("item", data.isClear && "done");
+  todo.textContent = `${i + 1}. ${data.title}`;
+  list.append(todo);
+});
